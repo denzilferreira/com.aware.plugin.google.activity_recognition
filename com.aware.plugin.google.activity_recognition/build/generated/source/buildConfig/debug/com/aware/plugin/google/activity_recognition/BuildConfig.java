bdd8f19fa9719b6518aa7854bb10334fd0ff0fb4
Denzil Ferreira <ferreiradenzil@gmail.com>
2014-11-15 11:43:59 -0500

    - Android 5.0 fixes.

--- com.aware.plugin.google.activity_recognition/build/generated/source/buildConfig/debug/com/aware/plugin/google/activity_recognition/BuildConfig.java
+++ com.aware.plugin.google.activity_recognition/build/generated/source/buildConfig/debug/com/aware/plugin/google/activity_recognition/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String PACKAGE_NAME = "com.aware.plugin.google.activity_recognition";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 13;
+  public static final int VERSION_CODE = 15;
   public static final String VERSION_NAME = "";
 }
