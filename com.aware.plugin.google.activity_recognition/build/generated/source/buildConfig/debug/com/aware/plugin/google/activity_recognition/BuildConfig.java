fa23c56f66e74c017d772e577d6521ab1c4aac83
Denzil Ferreira <ferreiradenzil@gmail.com>
2015-11-19 14:04:00 +0200

    Updated AWARE core Updated support libraries

--- com.aware.plugin.google.activity_recognition/build/generated/source/buildConfig/debug/com/aware/plugin/google/activity_recognition/BuildConfig.java
+++ com.aware.plugin.google.activity_recognition/build/generated/source/buildConfig/debug/com/aware/plugin/google/activity_recognition/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.aware.plugin.google.activity_recognition";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 31;
-  public static final String VERSION_NAME = "7.2";
+  public static final int VERSION_CODE = 32;
+  public static final String VERSION_NAME = "7.3";
 }
